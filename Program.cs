6864e13c88ca09b83854099f01ad4a42811949b2
AHMED-1739 <a85459866@gmail.com>
2024-11-19 23:42:49 +0300

    create (class Book) and new List

--- Program.cs
+++ Program.cs
@@ -6,10 +6,24 @@ using System.Threading.Tasks;
 
 namespace OOP_Progect_Library
 {
+    class Book
+    {
+        private readonly string Title;
+        private readonly string Auther;
+        public Book(string Title,string Auther)
+        {
+            this.Title = Title;
+            this.Auther = Auther;
+        }
+    }
+
+
     internal class Program
     {
         static void Main(string[] args)
         {
+          List <Book> library=new List<Book> ();
+
 
 
         }
