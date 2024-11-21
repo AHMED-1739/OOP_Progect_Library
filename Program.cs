25c4b0b7cd7b7ba9ef7b7879ac83520b03e4510f
AHMED-1739 <a85459866@gmail.com>
2024-11-21 20:54:57 +0300

    method

--- Program.cs
+++ Program.cs
@@ -24,29 +24,42 @@ namespace OOP_Progect_Library
     class Library
     {
         List<Book> books=new List<Book>();
+        //show the numbers of books in this library
+        public void NumberOfBook() => Console.WriteLine("This library contains {0} book", books.Count);
+
+        //check if the library empty
+        public bool IsEmpty() => books.Count == 0;
 
         //search method
-        public Book SearchByAuther(string Auther) 
+        public List<Book> SearchByAuther(string Auther) 
         {
+            List <Book> temp_books = new List<Book>();
+            if(books.Count == 0) return null;
+
             foreach(Book b in books)
-            {
-                if (b.Auther_ == Auther)
-                    return b;
-            }
-            return null;
+                if (b.Auther_ == Auther)                
+                    temp_books.Add(b);
+
+            if (temp_books.Count == 0) return null;
+            else return temp_books;
         }
-        public Book SearchByTitle(string Title)
+        public List<Book> SearchByTitle(string Title)
         {
+              List<Book> temp_books = new List<Book>();
+                if (books.Count == 0) return null;
             foreach (Book b in books)
-            {
+            
                 if (b.Title_ == Title)
-                    return b;
-            }
-            return null;
+                {
+                    temp_books.Add(b);
+                }
+               if(temp_books.Count == 0) return null;
+               else return temp_books;
         }
         public Book Search(string Title,string Auther)
         {
-         foreach(Book b in books)
+            if (books.Count == 0) return null;
+            foreach (Book b in books)
             {
                 if (b.Auther_ == Auther && b.Title_ == Title)
                     return b;
@@ -62,6 +75,9 @@ namespace OOP_Progect_Library
         //delet book from the library
         public void DeleteBook(string Auther,string Title)
         {
+            if (books.Count == 0) 
+                Console.WriteLine("the library does not contain anny books.");
+
             foreach (Book b in books)
                 if (b.Auther_ == Auther && b.Title_ == Title)
                 {
@@ -69,8 +85,8 @@ namespace OOP_Progect_Library
                     break;
                 }
         }
-        //show the numbers of books in this library
-        public void NumberOfBook() => Console.WriteLine("This library contains {0} book",books.Count);
+     
+  
         
     }
     internal class Program
