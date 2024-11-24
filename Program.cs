ef8eb82a6545d410bcab20e8a1d1f9ef765afcc5
AHMED-1739 <a85459866@gmail.com>
2024-11-25 02:55:50 +0300

    add now method to Library calss

--- Program.cs
+++ Program.cs
@@ -12,14 +12,14 @@ namespace OOP_Progect_Library
     {
         private readonly string Title;
         private readonly string Author;
-        public bool  IsAvailavle;
-        public string Auther_ { get => Author; }
+        public bool  IsAvailable;
+        public string Author_ { get => Author; }
         public string Title_ { get => Title; }
         public Book(string Title, string Author)
         {
             this.Title = Title;
             this.Author = Author;
-            IsAvailavle = true;
+            IsAvailable = true;
         }
     }
     class Library
@@ -28,20 +28,14 @@ namespace OOP_Progect_Library
         //show the numbers of books in this library
         public void NumberOfBook() => Console.WriteLine("This library contains {0} book", books.Count);
 
-
         //search method
-        public List<Book> SearchByAuther(string Author) 
+        public List<Book> SearchByAuthor(string Author)
         {
-            List <Book> temp_books = new List<Book>();
-
-
-            foreach(Book book in books)
-                if (book.Auther_ == Author)                
-                    temp_books.Add(book);
-
-            if (temp_books.Count == 0) return null;
-            else return temp_books;
+            List<Book> temp_books = (from book in books where book.Author_ == Author select book).ToList();
+            return temp_books;
         }
+
+
         public List<Book> SearchByTitle(string Title)
         {
               List<Book> temp_books = new List<Book>();
@@ -60,11 +54,30 @@ namespace OOP_Progect_Library
             List<Book> temp_books = new List<Book>();
             
             foreach (Book book in books)
-                if (book.Auther_ == Author && book.Title_ == Title)
+                if (book.Author_ == Author && book.Title_ == Title)
                         temp_books.Add(book);
             if (temp_books.Count == 0)
                 return null;
             else return temp_books;
+        }
+        //We will use this method if the user doesn't know whether what they know is the title or the author's name
+        public (List<Book>,List<Book>) SearchByBoth (string TitleOrAuthor)
+        {
+            if(string.IsNullOrWhiteSpace(TitleOrAuthor))
+                throw new Exception("there is no book whose author's name or title are nothing!!!!!");
+            
+            List<Book> MatchTheAuthorsName=new List<Book> ();
+            List<Book>MatchTheTitle=new List<Book> ();
+
+         for(int i=0;i<books.Count;i++)
+            {
+                if (books[i].Title_ == TitleOrAuthor)
+                    MatchTheTitle.Add(books[i]);
+                if (books[i].Author_==TitleOrAuthor)
+                    MatchTheAuthorsName.Add(books[i]);
+            }
+
+         return(MatchTheAuthorsName,MatchTheTitle);
         }
           //add book to the library
         public void Add(Book book)
@@ -81,18 +94,20 @@ namespace OOP_Progect_Library
                 return;
             }
 
-            foreach (Book book in books)
-                if (book.Auther_ == Auther && book.Title_ == Title)
+            for (int i = 0; i < books.Count; i++)
+            {
+                if (books[i].Author_ == Auther && books[i].Title_ == Title)
                 {
-                    books.Remove(book); 
+                    books.Remove(books[i]);
                     break;
                 }
+            }
         }
         // delete all the books by a specific author
         public void DeleteAllBookForAuther(string Author)
         {
             foreach(Book book in books)
-                if(book.Auther_ == Author)
+                if(book.Author_ == Author)
                     books.Remove(book);
         }
     }
@@ -100,25 +115,20 @@ namespace OOP_Progect_Library
     {
         static void Main(string[] args)
         {
-       
+            Library library = new Library();
+
+
+            Console.WriteLine("library catalogue");
+
+            while (true)
+            {
+                Console.WriteLine("Are you looking for specific book?");
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+            }
 
         }
+
     }
 }
